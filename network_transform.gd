4358718e567d12b37f16bdb3791badac9d54a6d1
Saracen <SaracenOne@gmail.com>
2019-06-29 14:37:54 +0100

    Silence warnings.

--- network_transform.gd
+++ network_transform.gd
@@ -10,6 +10,9 @@ func on_serialize(p_writer : network_writer_const, p_initial_state : bool) -> ne
 	if _entity_node.get_logic_node() == null:
 		_entity_node._ready()
 		
+	if p_initial_state:
+		pass
+		
 	var transform = _entity_node.get_logic_node().get_transform()
 	p_writer = write_transform(p_writer, transform)
 	
@@ -18,6 +21,9 @@ func on_serialize(p_writer : network_writer_const, p_initial_state : bool) -> ne
 func on_deserialize(p_reader : network_reader_const, p_initial_state : bool) -> network_reader_const:
 	var origin = Vector3(p_reader.get_float(), p_reader.get_float(), p_reader.get_float())
 	var quat = Quat(p_reader.get_float(), p_reader.get_float(), p_reader.get_float(), p_reader.get_float())
+	
+	if p_initial_state:
+		pass
 	
 	if is_network_master() == false:
 		_entity_node.get_logic_node().set_transform(Transform(Basis(quat), origin))
