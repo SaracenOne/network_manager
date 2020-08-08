05c71a5f035b17bcb7eb7ee8c88bc739be22432d
Saracen <SaracenOne@gmail.com>
2020-08-08 05:26:42 +0100

    Update network model function callback name

--- network_model.gd
+++ network_model.gd
@@ -3,8 +3,8 @@ extends NetworkLogic
 
 func on_serialize(p_writer: network_writer_const, p_initial_state: bool) -> network_writer_const:
 	if p_initial_state:
-		var name: String = entity_node.simulation_logic_node.get_model_path()
-		p_writer.put_8bit_pascal_string(name, true)
+		var path: String = entity_node.simulation_logic_node.get_model_path()
+		p_writer.put_8bit_pascal_string(path, true)
 
 	return p_writer
 
@@ -13,8 +13,8 @@ func on_deserialize(p_reader: network_reader_const, p_initial_state: bool) -> ne
 	received_data = true
 
 	if p_initial_state:
-		var name: String = p_reader.get_8bit_pascal_string(true)
-		entity_node.simulation_logic_node.load_model(name)
+		var path: String = p_reader.get_8bit_pascal_string(true)
+		entity_node.simulation_logic_node.set_model_from_path(path)
 
 	return p_reader
 
